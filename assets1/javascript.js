5344900004c1ce186113f2b91b145d7cd9d05903
paola <bernodiana@gmail.com>
2017-07-14 09:30:20 -0500

    Añadir

---
+++ assets1/javascript.js
@@ -0,0 +1,53 @@
+var productos = [];
+var records = document.getElementById('records');
+
+function Producto(compra) {
+  this.compra = compra.toLowerCase();
+  this.producttID = (productos.length + 1);
+  }
+
+
+
+
+Producto.prototype.toHTML = function () {
+  var html = '';
+  html += this.compra.toUpperCase() + '<br>';
+  html += '<br><br>';
+  return html;
+}
+
+
+
+function mergeHTML (){
+  var html = '';
+  for (i=0; i<productos.length; i++){
+    html += productos[i].toHTML();
+  }
+  return html;
+}
+
+
+
+function printHTML (html){
+  records.innerHTML = '';
+  records.innerHTML = html;
+}
+
+
+
+
+var addCompra = document.getElementById('nuevafruta');
+addCompra.onclick = function() {
+  var compra = prompt('Ingrese Fruta');
+  var product  = new Producto (compra);
+  productos.push(product);
+  printHTML(product.toHTML());
+};
+
+
+
+
+var printAll = document.getElementById('print');
+printAll.onclick = function() {
+  printHTML(mergeHTML());
+}
